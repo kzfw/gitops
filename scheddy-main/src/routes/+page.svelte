a922f2c03472fefcf9ce26d5c9ce54b50428bedd
bkinney272 <111651918+bkinney272@users.noreply.github.com>
2025-01-06 16:58:57 -0600

    Initial Deployment
    Still needs to be configured.  Need to set up SQL and link everything together.  Once complete we will need final configurations.

---
+++ scheddy-main/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import {
+		PUBLIC_FACILITY_NAME,
+		PUBLIC_VATSIM_OAUTH_BASE,
+		PUBLIC_VATSIM_OAUTH_CLIENT_ID,
+		PUBLIC_VATSIM_OAUTH_REDIRECT_URL
+	} from '$env/static/public';
+	import Button from '$lib/ui/Button.svelte';
+	import Card from '$lib/ui/Card.svelte';
+	import { version } from '$app/environment';
+	import { HeartIcon } from 'lucide-svelte';
+</script>
+
+<div class="min-w-screen min-h-screen flex flex-col align-middle justify-center items-center">
+	<Card>
+		<h1 class="font-bold text-2xl text-center">{PUBLIC_FACILITY_NAME} - Scheddy</h1>
+
+		<Button
+			href="{PUBLIC_VATSIM_OAUTH_BASE}/oauth/authorize?response_type=code&client_id={PUBLIC_VATSIM_OAUTH_CLIENT_ID}&redirect_uri={PUBLIC_VATSIM_OAUTH_REDIRECT_URL}"
+		>
+			Login with VATSIM
+		</Button>
+
+		<a
+			target="_blank"
+			href="https://github.com/ZTL-ARTCC/scheddy"
+			class="block hover:underline text-sm text-slate-500"
+			>scheddy v{version} - built with <HeartIcon class="inline w-5 h-5 align-top" /> by the ZTL ARTCC</a
+		>
+	</Card>
+</div>
